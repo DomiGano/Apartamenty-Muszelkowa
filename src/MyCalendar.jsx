fd2994057551cba51e679beceefdb10a597c3269
DomiGano <ganowiczdominik@gmail.com>
2023-10-11 23:51:23 +0200

    Adding new events and form adjustments

--- src/MyCalendar.jsx
+++ src/MyCalendar.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'moment/locale/pl';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 import { db } from './firebase';
 
 export const MyCalendar = (props) => {
   const [myEvents, setMyEvents] = useState([]);
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
 
 
 //Firebase pobieranie danyc z bazy danych oraz konwersja tych danych do state w celu późniejszego ich otworzenia w kalendarzu
@@ -21,8 +23,8 @@ export const MyCalendar = (props) => {
             events.push({
               id: doc.id,
               title: 'REZERWACJA',
-              start: new Date(data.start.toDate()),
-              end: new Date(data.end.toDate()),
+              start: new Date(data.start),
+              end: new Date(data.end),
             });
           });
   
@@ -34,8 +36,32 @@ export const MyCalendar = (props) => {
     };
   
     fetchData();
-  }, []);  
+  }, [myEvents]);  
 
+// Dodawanie danych do bazy danych 
+
+const addToCalendar = () => {
+  addDoc(collection(db, "events"), {
+    start: startDate,
+    end: endDate,
+  })
+    .then((docRef) => {
+      alert("Termin został zarezerwowany");
+    })
+    .catch((e) => {
+      console.error("Error adding document: ", e);
+    });
+    setEndDate('')
+    setStartDate('')
+};
+
+const handleStartDateChange = (event) => {
+  setStartDate(event.target.value);
+};
+
+const handleEndDateChange = (event) => {
+  setEndDate(event.target.value);
+};
 
   const messages = {
     previous: 'Poprzedni',
@@ -60,6 +86,24 @@ export const MyCalendar = (props) => {
             style={{ height: 500, width: '100%', backgroundColor: '#FFF', padding: '1.5em' }}
           />
         </div>
+        <form>
+        <h1>REZERWACJE</h1>
+      <br></br>
+      <label> Początek wyjazdu </label>
+      <input
+        type='date'
+        value={startDate}
+        onChange={handleStartDateChange}
+      />
+      <br></br>
+      <label> Koniec wyjazdu </label>
+      <input
+        type='date'
+        value={endDate}
+        onChange={handleEndDateChange}
+      />
+    </form>
+    <button onClick={() => addToCalendar()}>REZERWACJA</button>
       </div>
     </div>
   );
